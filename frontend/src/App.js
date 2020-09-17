9c938f8d7f262850a4f87b3519db83a89c0c822c
Bao Nguyen <hoaibaonguyen1999@gmail.com>
2020-09-17 12:13:47 -0700

    Add deployment-sensitive files and modify frontend code accordingly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Suspense } from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -6,24 +6,18 @@ import {
     Switch,
 } from "react-router-dom";
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Authenticate";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
 import { AuthContext } from "./shared/context/auth-context";
-import { useAuth } from './shared/hooks/auth-hook'
+import { useAuth } from './shared/hooks/auth-hook';
+
+const Users = React.lazy(() => import('./user/pages/Users'));
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'));
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'));
+const Auth = React.lazy(() => import('./user/pages/Authenticate'));
 
 const App = () => {
-    useEffect(() => {
-        const index = window.document.getElementsByTagName('script')[0];
-        let script = window.document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`;
-        script.async = true;
-        script.defer = true;
-        index.parentNode.insertBefore(script, index);
-    }, []);
 
     const { token, login, logout, userId } = useAuth();
 
@@ -83,7 +77,7 @@ const App = () => {
         >
             <Router>
                 <MainNavigation />
-                <main>{routes}</main>
+                <main><Suspense fallback={<div className="center"><LoadingSpinner /></div>}>{routes}</Suspense></main>
             </Router>
         </AuthContext.Provider>
     );
--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -7,43 +7,43 @@ import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
-  const [loadedPlaces, setLoadedPlaces] = useState();
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  const userId = useParams().userId;
+    const userId = useParams().userId;
 
-  useEffect(() => {
-    const fetchPlaces = async () => {
-      try {
-        const responseData = await sendRequest(
-          `http://localhost:5000/api/places/user/${userId}`
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                const responseData = await sendRequest(
+                    `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
+                );
+                setLoadedPlaces(responseData.places);
+            } catch (error) { }
+        };
+
+        fetchPlaces();
+    }, [sendRequest, userId]);
+
+    const placeDeletedHandler = (deletedPlaceId) => {
+        setLoadedPlaces((prevPlaces) =>
+            prevPlaces.filter((place) => place.id !== deletedPlaceId)
         );
-        setLoadedPlaces(responseData.places);
-      } catch (error) {}
     };
 
-    fetchPlaces();
-  }, [sendRequest, userId]);
-
-  const placeDeletedHandler = (deletedPlaceId) => {
-    setLoadedPlaces((prevPlaces) =>
-      prevPlaces.filter((place) => place.id !== deletedPlaceId)
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedPlaces && (
+                <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
+            )}
+        </React.Fragment>
     );
-  };
-
-  return (
-    <React.Fragment>
-      <ErrorModal error={error} onClear={clearError} />
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
-        </div>
-      )}
-      {!isLoading && loadedPlaces && (
-        <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
-      )}
-    </React.Fragment>
-  );
 };
 
 export default UserPlaces;
